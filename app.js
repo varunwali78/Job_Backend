92027032449f3304ff7c4492acd63a2a17e78b34
varunwali <varunwali36@gmail.com>
2024-08-16 15:06:02 +0530

    commit

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import jobRouter from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
+import { config } from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: "https://job-portal-frontend-v76k.onrender.com",
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.set("trust proxy", 1);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API running",
+  });
+});
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
